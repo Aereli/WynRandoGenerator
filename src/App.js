91caabcfe9dbf5b57ccad9b51b87322136e0cc41
Eric <Aerelioyanadel@gmail.com>
2020-04-01 21:57:31 -0400

    create minor styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from "react"
 import "../src/styles/App.css"
 import Students from "./utils/students"
-import { useState } from 'react'
+import { useState } from "react"
 
 function App() {
-  const [randomStudent, setRandomStudent] = useState('')
+  const [randomStudent, setRandomStudent] = useState("")
 
   const data = Students()
   if (!data) return <p>Loading...</p>
 
   function handleClick() {
-    const randomStudentNumber = Math.floor(Math.random()*data.data.length)
+    const randomStudentNumber = Math.floor(Math.random() * data.data.length)
     const studentFirstName = data.data[randomStudentNumber].firstName
     const studentLastName = data.data[randomStudentNumber].lastName
     setRandomStudent(studentFirstName + " " + studentLastName)
+    // console.log(data.data[0])
   }
 
-  console.log(data)
   return (
-    <div>
-      <h1>Hello</h1>
-      <p>Students:</p>
-      <button onClick={handleClick}>Generate Random Student</button>
-      {randomStudent}
+    <div className="background">
+      <div className="student-container">
+        <h1>Random Student Generator!</h1>
+        <button className="student-button" onClick={handleClick}>
+          Generate Random Student
+        </button>
+        <div className="student">{randomStudent}</div>
+      </div>
     </div>
   )
 }
