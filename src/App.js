6f70ba253ef3563eb3d223c8539ab81831d0d5a6
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-04-01 20:48:08 -0400

    added random student button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from "react"
 import "../src/styles/App.css"
 import Students from "./utils/students"
+import { useState } from 'react'
 
 function App() {
+  const [randomStudent, setRandomStudent] = useState('')
+
   const data = Students()
   if (!data) return <p>Loading...</p>
+
+  function handleClick() {
+    const randomStudentNumber = Math.floor(Math.random()*data.data.length)
+    const studentFirstName = data.data[randomStudentNumber].firstName
+    const studentLastName = data.data[randomStudentNumber].lastName
+    setRandomStudent(studentFirstName + " " + studentLastName)
+  }
 
   console.log(data)
   return (
     <div>
       <h1>Hello</h1>
       <p>Students:</p>
+      <button onClick={handleClick}>Generate Random Student</button>
+      {randomStudent}
     </div>
   )
 }
